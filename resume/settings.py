8abc07969533d86faf95bf4e5861a720032673dc
Alekxandr <alkv84@yandex.ru>
2018-01-05 19:50:34 +0200

    2

--- resume/settings.py
+++ resume/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'resume.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'resume'],
+        'DIRS': ['templates', 'resume/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
