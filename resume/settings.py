7f2c8668280146b808e22b13ff1d4c865be36a62
Alekxandr <alkv84@yandex.ru>
2018-01-30 14:09:16 +0200

    add index

--- resume/settings.py
+++ resume/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'resume.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'resume/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
